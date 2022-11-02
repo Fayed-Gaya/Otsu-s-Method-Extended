c3d9589fe270283d250e30f58d383374cd81b450
Dave Hauss <83170802+dhauss@users.noreply.github.com>
2022-11-02 16:11:02 -0400

    Update Otsu's Method Extended.py

--- Otsu's Method Extended.py
+++ Otsu's Method Extended.py
@@ -1,10 +1,20 @@
 # 𝐼 = 𝑅ound(0.299𝑅 + 0.587𝐺 + 0.114𝐵)
 from PIL import Image as im
 
+
 def main():
     with im.open("input1.bmp") as input_image:
-        output_image = (input_image.convert("L"))
-        output_image.save("output1.bmp")
+        width, height = input_image.size
+        output_image = im.new('L', (width, height))
+        pixel_map = output_image.load()
+
+        # convert to grayscale
+        for i in range(0, width):
+            for j in range(height):
+                r, g, b = input_image.getpixel((i, j))
+                grayscale = (0.299 * r + 0.587 * g + 0.114 * b)
+                pixel_map[i, j] = (int(grayscale))
+                output_image.save("output1.bmp")
 
 
 if __name__ == "__main__":
